8ce973d6ce88e6f2ee2a5e0392caa1e5cae5da48
olafrank <olafrnk@gmail.com>
2023-03-01 01:47:18 +0100

    url updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -51,7 +51,7 @@ function App() {
 
       // save user to mongodb
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://otech-realestate.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -122,7 +122,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://otech-realestate.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
